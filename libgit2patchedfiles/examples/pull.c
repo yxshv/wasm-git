91ff6f34f61e417a2c68d5750a0aa35a34efa1cd
yxshv <yashverma30115@gmail.com>
2024-01-24 21:47:49 +0530

    add pull command

---
+++ libgit2patchedfiles/examples/pull.c
@@ -0,0 +1,100 @@
+#include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <git2.h>
+
+void print_error(const char *action, int error_code) {
+    const git_error *error = giterr_last();
+    fprintf(stderr, "%s failed with error %d: %s\n", action, error_code, error->message);
+}
+
+int lg2_pull(git_repository *repo, int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <repository_path> <branch_name>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char *repository_path = argv[1];
+    const char *branch_name = argv[2];
+
+    git_libgit2_init();
+
+    // Open the repository
+    if (repo == NULL && git_repository_open(&repo, repository_path) != GIT_OK) {
+        print_error("git_repository_open", -1);
+        git_libgit2_shutdown();
+        return EXIT_FAILURE;
+    }
+
+    // Fetch changes from the remote repository
+    git_remote *remote = NULL;
+    if (git_remote_lookup(&remote, repo, "origin") != GIT_OK) {
+        print_error("git_remote_lookup", -1);
+        git_repository_free(repo);
+        git_libgit2_shutdown();
+        return EXIT_FAILURE;
+    }
+
+    git_fetch_options fetch_options = GIT_FETCH_OPTIONS_INIT;
+    if (git_remote_fetch(remote, NULL, &fetch_options, NULL) != GIT_OK) {
+        print_error("git_remote_fetch", -1);
+        git_remote_free(remote);
+        git_repository_free(repo);
+        git_libgit2_shutdown();
+        return EXIT_FAILURE;
+    }
+
+    // Get the local branch
+    git_reference *local_branch = NULL;
+    if (git_branch_lookup(&local_branch, repo, branch_name, GIT_BRANCH_LOCAL) != GIT_OK) {
+        print_error("git_branch_lookup", -1);
+        git_remote_free(remote);
+        git_repository_free(repo);
+        git_libgit2_shutdown();
+        return EXIT_FAILURE;
+    }
+
+    // Get the upstream branch
+    git_reference *upstream_branch = NULL;
+    if (git_branch_upstream(&upstream_branch, local_branch) != GIT_OK) {
+        print_error("git_branch_upstream", -1);
+        git_reference_free(local_branch);
+        git_remote_free(remote);
+        git_repository_free(repo);
+        git_libgit2_shutdown();
+        return EXIT_FAILURE;
+    }
+
+    // Merge changes from the upstream branch
+    git_merge_options merge_options = GIT_MERGE_OPTIONS_INIT;
+    if (git_merge(repo, upstream_branch, &merge_options, NULL) != GIT_OK) {
+        if (git_index_has_conflicts(git_repository_index(repo))) {
+            printf("Merge conflicts detected. Please resolve conflicts and commit.\n");
+        } else {
+            print_error("git_merge", -1);
+        }
+
+        git_reference_free(upstream_branch);
+        git_reference_free(local_branch);
+        git_remote_free(remote);
+        git_repository_free(repo);
+        git_libgit2_shutdown();
+        return EXIT_FAILURE;
+    }
+
+    // Clean up
+    git_reference_free(upstream_branch);
+    git_reference_free(local_branch);
+    git_remote_free(remote);
+
+    // If repo was opened inside this function, free it
+    if (argv[1] != NULL) {
+        git_repository_free(repo);
+    }
+
+    git_libgit2_shutdown();
+
+    printf("Pull successful.\n");
+    return EXIT_SUCCESS;
+}
+
--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ cp -r libgit2patchedfiles/src/* libgit2/src/libgit2/
 echo 'set(CMAKE_C90_STANDARD_COMPILE_OPTION "-std=gnu90")' >> libgit2/examples/CMakeLists.txt
 echo 'set(CMAKE_C90_STANDARD_COMPILE_OPTION "-std=gnu90")' >> libgit2/src/libgit2/CMakeLists.txt
 echo 'set(CMAKE_C90_STANDARD_COMPILE_OPTION "-std=gnu90")' >> libgit2/src/util/CMakeLists.txt
-sed -i 's/GIT_PACK_FILE_MODE 0444/GIT_PACK_FILE_MODE 0644/g' libgit2/src/libgit2/pack.h
-sed -i 's/GIT_OBJECT_FILE_MODE 0444/GIT_OBJECT_FILE_MODE 0644/g' libgit2/src/libgit2/odb.h
+sed 's/GIT_PACK_FILE_MODE 0444/GIT_PACK_FILE_MODE 0644/g' libgit2/src/libgit2/pack.h
+sed 's/GIT_OBJECT_FILE_MODE 0444/GIT_OBJECT_FILE_MODE 0644/g' libgit2/src/libgit2/odb.h
