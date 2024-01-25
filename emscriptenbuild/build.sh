988b602c76508472e1ff88def18d52caa02dc75a
yxshv <yashverma30115@gmail.com>
2024-01-25 13:50:31 +0530

    fix file name

--- emscriptenbuild/build.sh
+++ emscriptenbuild/build.sh
@@ -39,4 +39,4 @@ emcmake cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_FLAGS="$EXTRA_CMAKE_C_FLA
 emmake make lg2
 
 
-sed -e "s/fs.mkdirSync(path, node.mode)/fs.mkdirSync(path, {recursive:true,mode:node.mode})/g" -e "s/fs.writeFileSync(path, '', { mode: node.mode });/fs.writeFileSync(path, '', { mode: node.mode, recursive: true });/g" ../emcriptenbuild/libgit2/examples/lg2.js
+sed -e "s/fs.mkdirSync(path, node.mode)/fs.mkdirSync(path, {recursive:true,mode:node.mode})/g" -e "s/fs.writeFileSync(path, '', { mode: node.mode });/fs.writeFileSync(path, '', { mode: node.mode, recursive: true });/g" ./libgit2/examples/lg2.js
