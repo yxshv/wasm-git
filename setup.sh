5bb1748e94a4462c2fd545b1e758c4efed43bc70
yxshv <yashverma30115@gmail.com>
2024-01-25 17:42:10 +0530

    fix command on mac

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 curl -L https://github.com/libgit2/libgit2/archive/refs/tags/v1.7.1.tar.gz --output libgit2.tar.gz
 tar -xzf libgit2.tar.gz
+rm -rf libgit2
 mv libgit2-1.7.1 libgit2
 rm libgit2.tar.gz
 rm libgit2/src/libgit2/transports/http.c
@@ -8,5 +9,9 @@ cp -r libgit2patchedfiles/src/* libgit2/src/libgit2/
 echo 'set(CMAKE_C90_STANDARD_COMPILE_OPTION "-std=gnu90")' >> libgit2/examples/CMakeLists.txt
 echo 'set(CMAKE_C90_STANDARD_COMPILE_OPTION "-std=gnu90")' >> libgit2/src/libgit2/CMakeLists.txt
 echo 'set(CMAKE_C90_STANDARD_COMPILE_OPTION "-std=gnu90")' >> libgit2/src/util/CMakeLists.txt
-sed 's/GIT_PACK_FILE_MODE 0444/GIT_PACK_FILE_MODE 0644/g' libgit2/src/libgit2/pack.h
-sed 's/GIT_OBJECT_FILE_MODE 0444/GIT_OBJECT_FILE_MODE 0644/g' libgit2/src/libgit2/odb.h
+
+sed 's/GIT_PACK_FILE_MODE 0444/GIT_PACK_FILE_MODE 0644/g' libgit2/src/libgit2/pack.h >> libgit2/src/libgit2/pack.h.tmp
+sed 's/GIT_OBJECT_FILE_MODE 0444/GIT_OBJECT_FILE_MODE 0644/g' libgit2/src/libgit2/odb.h >>libgit2/src/libgit2/odb.h.tmp
+
+mv libgit2/src/libgit2/pack.h.tmp libgit2/src/libgit2/pack.h
+mv libgit2/src/libgit2/odb.h.tmp libgit2/src/libgit2/odb.h
