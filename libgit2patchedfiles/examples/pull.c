f7a34b792dbd2d0b0004ee4c4db8baec8e7d7f17
yxshv <yashverma30115@gmail.com>
2024-01-26 19:07:15 +0530

    fix pull command

--- libgit2patchedfiles/examples/pull.c
+++ libgit2patchedfiles/examples/pull.c
@@ -1,12 +1,18 @@
 #include <git2.h>
 #include <stdio.h>
 
+void check_error(int error, const char *message) {
+    if (error != 0) {
+        fprintf(stderr, "%s: %s\n", message, git_error_last());
+        exit(1);
+    }
+}
+
 int pull(git_repository *repo) {
     int error = 0;
     git_remote *remote = NULL;
     git_fetch_options fetch_opts = GIT_FETCH_OPTIONS_INIT;
     git_merge_options merge_opts = GIT_MERGE_OPTIONS_INIT;
-    const char *remote_url = NULL;
     const char *remote_branch = NULL;
     const char *local_branch = NULL;
 
@@ -15,15 +21,17 @@ int pull(git_repository *repo) {
     check_error(error, "Failed to get current branch");
 
     // Get the remote of the current branch
-    error = git_branch_upstream(&remote, repo, remote_branch);
-    if (error == GIT_ENOTFOUND) {
-        printf("Current branch has no upstream branch.\n");
-        return 1;
-    }
+    git_buf *upstream_branch = NULL;
+
+		error = git_branch_upstream(&upstream_branch, repo);
+		if (error == GIT_ENOTFOUND) {
+				printf("Current branch has no upstream branch.\n");
+				return 1;
+		}
     check_error(error, "Failed to get upstream remote");
 
     // Get the remote URL and remote branch name
-    error = git_remote_url(&remote_url, remote);
+    const char *remote_url = git_remote_url(remote);
     check_error(error, "Failed to get remote URL");
     error = git_branch_name(&remote_branch, remote_branch);
     check_error(error, "Failed to get remote branch name");
@@ -33,7 +41,7 @@ int pull(git_repository *repo) {
 
 
 		// Look up the remote
-    error = git_remote_lookup(&remote, repo, remote_url);
+    error = git_remote_lookup(&remote, repo, upstream_branch);
     check_error(error, "Failed to find remote");
 
     // Fetch from the remote
@@ -41,8 +49,7 @@ int pull(git_repository *repo) {
     check_error(error, "Failed to fetch from remote");
 
     // Merge the remote branch into the local branch
-    error = git_merge(repo, &merge_opts, (const git_annotated_commit **) &remote_branch, 1);
-		error = git_merge(repo, (const git_annotated_commit **), &remote_branch, 1 );
+		error = git_merge(repo, (const git_annotated_commit **) &remote_branch, 1, &merge_opts, NULL);
 
     // Handle merge scenarios
     if (error == GIT_MERGE_ANALYSIS_UP_TO_DATE) {
@@ -60,13 +67,6 @@ cleanup:
     git_remote_free(remote);
     git_repository_free(repo);
     return error;
-}
-
-void check_error(int error, const char *message) {
-    if (error != 0) {
-        fprintf(stderr, "%s: %s\n", message, giterr_last());
-        exit(1);
-    }
 }
 
 int lg2_pull(git_repository *repo,int argc, char **argv) {
