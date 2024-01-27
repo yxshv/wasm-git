3d4d8ce777a390eab500a82895d62536b380cdbb
yxshv <yashverma30115@gmail.com>
2024-01-27 23:10:32 +0530

    final fix

--- libgit2patchedfiles/examples/pull.c
+++ libgit2patchedfiles/examples/pull.c
@@ -12,19 +12,55 @@ void check_error(int error, const char *message) {
 
 int lg2_pull(git_repository *repo, int argc, char **argv) {
 
+	int error = 0;
+
 	char *_argv[2];
 	argv[0] = "fetch";
-	argv[1] = ".";
+	argv[1] = "origin";
 
 	lg2_fetch(repo, 2, _argv);
 
-	char *__argv[2];
-	argv[0] = "merge";
-	argv[1] = "origin/master";
 
-	lg2_merge(repo, 2, __argv);
+	git_reference *head;
+	const char *branch = NULL;
+
+	error = git_repository_head(&head, repo);
+	
+  if (error == GIT_EUNBORNBRANCH || error == GIT_ENOTFOUND) {
+    branch = NULL;
+	} else if (!error) {
+    branch = git_reference_shorthand(head);
+
+		char *name;
+
+		// Allocate memory for the formatted name
+		name = malloc(strlen("origin/") + strlen(branch) + 1);
+		if (name == NULL) {
+				// Handle memory allocation error
+				fprintf(stderr, "Memory allocation failed\n");
+				goto cleanup;
+		}
+
+		// Create the formatted name
+		sprintf(name, "origin/%s", branch);
+
+		char *__argv[2];
+		__argv[0] = "merge";
+		__argv[1] = name;
+
+		lg2_merge(repo, 2, __argv);
+
+  } else {
+    check_lg2(error, "failed to get current branch", NULL);	
+	}
 
 	printf("\nSuccessfully pulled!\n");
+
+cleanup:
+	git_reference_free(head);
+	git_repository_free(repo);
+
+
 
 }
 
